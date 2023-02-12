faf297f0978c183cecd9e839a2c7bb879bcb4c06
Baracho <baracholss@gmail.com>
2023-02-12 08:02:56 -0300

    estrutura do projeto feita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/admin/pages/home/home.component';
+import { SignComponent } from './core/components/auth/pages/sign/sign.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule,
